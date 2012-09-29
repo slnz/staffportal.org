9a983d5fa42d0f1c553f17382e216b08147500b0
Tataihono Nikora <tataihono.nikora@gmail.com>
2012-09-29 23:51:41 +1200

    coeditor production problems

--- app/controllers/week6_controller.rb
+++ app/controllers/week6_controller.rb
@@ -1,5 +1,5 @@
 class Week6Controller < InheritedResources::Base
   def show
-    redirect_to 'staff/bootcamp', :notice => "Saved Week 6 Settings"
+    redirect_to '/staff/bootcamp', :notice => "Saved 6 Weeks to Bootcamp Settings"
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Staff::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( auth.css staff.css bootcamp.css )
+  config.assets.precompile += %w( auth.css staff.css bootcamp.css ckeditor/* )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
