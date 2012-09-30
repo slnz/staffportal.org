d39158eaa99313d28562be99a040af5eff0695bb
Tataihono Nikora <tataihono.nikora@gmail.com>
2012-09-30 15:05:54 +1300

    added error checking for field data

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,16 +4,21 @@ class ContactsController < InheritedResources::Base
     redirect_to '/staff/bootcamp', :notice => "Contact Removed"
   end
   def create
-    params[:contact][:first_name] = params[:contact][:first_name].capitalize
-    params[:contact][:last_name] = params[:contact][:last_name].capitalize
-    @exists = Contact.where(:first_name => params[:contact][:first_name],:last_name => params[:contact][:last_name], :phone => params[:contact][:phone]).first
-    if @exists.nil?
-      @exists = Contact.where(:phone => params[:contact][:phone]).first
-    end
-    if @exists.nil?
-      super
+    if params[:contact][:first_name].empty? or params[:contact][:last_name].empty? or params[:contact][:phone].empty?
+      redirect_to '/staff/bootcamp', :notice => "Missing Contact Field Data".html_safe
     else
-      redirect_to '/staff/bootcamp', :notice => "Contact already claimed by <a href=\"mailto:#{@exists.user.username}\">#{@exists.user.username}</a>".html_safe
+      params[:contact][:first_name] = params[:contact][:first_name].capitalize
+      params[:contact][:last_name] = params[:contact][:last_name].capitalize
+      params[:contact][:phone] = params[:contact][:phone].gsub(/[^0-9]/, "")
+      @exists = Contact.where(:first_name => params[:contact][:first_name],:last_name => params[:contact][:last_name], :phone => params[:contact][:phone]).first
+      if @exists.nil?
+        @exists = Contact.where(:phone => params[:contact][:phone]).first
+      end
+      if @exists.nil?
+        super
+      else
+        redirect_to '/staff/bootcamp', :notice => "Contact already claimed by <a href=\"mailto:#{@exists.user.username}\">#{@exists.user.username}</a>".html_safe
+      end
     end
   end
   def show
