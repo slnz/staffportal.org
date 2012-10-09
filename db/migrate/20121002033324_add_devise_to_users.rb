905d2bb8e90fcfeb909a86dd015bf9420b50d1bd
Tataihono Nikora <tataihono.nikora@gmail.com>
2012-10-09 22:52:13 +1300

    removed already existing table definition

--- db/migrate/20121002033324_add_devise_to_users.rb
+++ db/migrate/20121002033324_add_devise_to_users.rb
@@ -1,6 +1,5 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :username, :string, :null => false
     add_column :users, :admin, :string
     add_index :users, :username, :unique => true
   end
