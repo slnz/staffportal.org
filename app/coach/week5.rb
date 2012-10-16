e1ac09400a5b59d4362e9cdfebf76c3eb3886bfe
Tataihono Nikora <tataihono.nikora@gmail.com>
2012-10-17 12:38:52 +1300

    fixed bug with week6 and added admin for week5

--- app/coach/week5.rb
+++ app/coach/week5.rb
@@ -1,4 +1,4 @@
- ActiveAdmin.register Week6, :namespace => :coach do
+ActiveAdmin.register Week5, :namespace => :coach do
   controller do
     authorize_resource
 
@@ -25,9 +25,9 @@
     redirect_to collection_path, :notice => "Unconfirmed selected!"
   end
 
-  menu :parent => "Bootcamp", :label => "6 Weeks Until Bootcamp"
+  menu :parent => "Bootcamp", :label => "5 Weeks Until Bootcamp"
 
-  index :title => "6 Weeks Until Bootcamp" do |week6|
+  index :title => "5 Weeks Until Bootcamp" do |week5|
     selectable_column
     column ("Status")   {|tasklist| status_tag(tasklist.completed) }
     column :user
@@ -39,75 +39,65 @@
         else
           status_tag("UNCONFIRMED", "orange")
         end }
+    column ("HR") { |tasklist|
+        if tasklist.confirmed_hr
+          status_tag("CONFIRMED", "green")
+        else
+          status_tag("UNCONFIRMED", "orange")
+        end }
     default_actions
   end
 
-  show :title => :username do |week6|
+  show :title => :username do |week5|
     attributes_table do
       row ("listen_to_attitude_message") {
-        if week6.listen_to_attitude_message
+        if week5.listen_to_attitude_message
           status_tag("COMPLETED")
         else
           status_tag("IN PROGRESS")
         end }
       row ("memorize_prayer_sheet_passage") {
-        if week6.memorize_prayer_sheet_passage
+        if week5.memorize_prayer_sheet_passage
           status_tag("COMPLETED")
         else
           status_tag("IN PROGRESS")
         end }
       row ("prayer_sheet") {
-        if week6.prayer_sheet
+        if week5.prayer_sheet
           status_tag("COMPLETED")
         else
           status_tag("IN PROGRESS")
         end }
-      row ("review_how_to_get_contacts_faster_than_you_can_use_them") {
-        if week6.review_how_to_get_contacts_faster_than_you_can_use_them
+      row ("review_non_negotiables_of_effective_mpd") {
+        if week5.review_non_negotiables_of_effective_mpd
           status_tag("COMPLETED")
         else
           status_tag("IN PROGRESS")
         end }
-      row ("review_purpose_of_dmpd_training") {
-        if week6.review_purpose_of_dmpd_training
+      row ("recite_passage_from_dmpd_prayer_sheet") {
+        if week5.recite_passage_from_dmpd_prayer_sheet
           status_tag("COMPLETED")
         else
           status_tag("IN PROGRESS")
         end }
       row ("share_call_testimony") {
-        if week6.share_call_testimony
+        if week5.share_call_testimony
           status_tag("COMPLETED")
         else
           status_tag("IN PROGRESS")
         end }
       row ("share_salvation_testimony") {
-        if week6.share_salvation_testimony
+        if week5.share_salvation_testimony
           status_tag("COMPLETED")
         else
           status_tag("IN PROGRESS")
         end }
-      row ("call_testimony") {
-        if week6.call_testimony
-          status_tag("COMPLETED")
-        else
-          status_tag("IN PROGRESS")
-        end }
-      row ("salvation_testimony") {
-        if week6.salvation_testimony
-          status_tag("COMPLETED")
-        else
-          status_tag("IN PROGRESS")
-        end }
-      row ("discipleship_story_1") { week6.discipleship_story_1.html_safe unless week6.discipleship_story_1.blank? }
-      row ("discipleship_story_2") { week6.discipleship_story_2.html_safe unless week6.discipleship_story_2.blank? }
-      row ("discipleship_story_3") { week6.discipleship_story_3.html_safe unless week6.discipleship_story_3.blank? }
-      row ("victory_story_1") { week6.victory_story_1.html_safe unless week6.victory_story_1.blank? }
-      row ("victory_story_2") { week6.victory_story_2.html_safe unless week6.victory_story_2.blank? }
+      row ("missionary_profile") { week5.missionary_profile.html_safe unless week5.missionary_profile.blank? }
       row ("confirmed_coach") {
-        if week6.confirmed_coach
-          status_tag("CONFIRMED", "green")
+        if week5.confirmed_coach
+          status_tag("COMPLETED")
         else
-          status_tag("UNCONFIRMED", "orange")
+          status_tag("IN PROGRESS")
         end }
     end
     active_admin_comments
