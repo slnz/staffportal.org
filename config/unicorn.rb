ebb5fa866bd7d64b824d7d6118765ed44536567d
Tataihono Nikora <tataihono.nikora@gmail.com>
2012-11-02 08:15:01 +1300

    redis changes for unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -27,7 +27,7 @@ after_fork do |server, worker|
 
   # If you are using Redis but not Resque, change this
   if defined?(Resque)
-    Resque.redis = ENV['REDIS_URI']
+    Resque.redis = ENV["REDISTOGO_URL"]
     Rails.logger.info('Connected to Redis')
   end
 end
