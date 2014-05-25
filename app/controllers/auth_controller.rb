8b62cefc4bf6a3d07a0632920f144a6199257b6e
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-05-26 11:16:09 +1200

    cas fixes

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,12 +1,9 @@
 class AuthController < ApplicationController
-  before_filter :authenticate_user!, except: [:home, :relay]
+  before_filter :authenticate_user!, except: [:home]
   def home
     unless current_user.nil?
       redirect_to '/staff'
     end
-  end
-
-  def relay
   end
 
   def key
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -36,4 +36,6 @@ Devise.setup do |config|
   config.reset_password_within = 6.hours
 
   config.sign_out_via = :delete
+
+  config.cas_base_url = 'https://thekey.me/cas/'
 end
