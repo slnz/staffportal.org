f2871aa0089f1b5d3b8f74ce4fa6e58c5e02c877
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-09-25 18:43:05 +1200

    tidy taskset formatting

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,4 +3,7 @@ class Assignment < ActiveRecord::Base
   has_many :user_assignments, dependent: :destroy
   belongs_to :taskset
   accepts_nested_attributes_for :user_assignments
+  validates :name, presence: true
+  validates :taskset, presence: true
+  validates :kind, presence: true
 end
