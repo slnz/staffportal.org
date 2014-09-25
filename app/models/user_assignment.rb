bc5ec65ce7000542f5dd730707ecae0d45c12607
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-09-25 18:01:03 +1200

    update user_assignment model with attachment

--- app/models/user_assignment.rb
+++ app/models/user_assignment.rb
@@ -2,4 +2,6 @@ class UserAssignment < ActiveRecord::Base
   belongs_to :user
   belongs_to :assignment
   belongs_to :taskset
+  has_attached_file :answer_upload
+  do_not_validate_attachment_file_type :answer_upload
 end
