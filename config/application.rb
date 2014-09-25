ee280ad91d95582d6359320e09181c9d67292fd9
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-09-25 17:54:28 +1200

    uploads for tasksets

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,14 @@ module Staff
     config.assets.initialize_on_precompile = false
 
     config.i18n.enforce_available_locales = false
+
+    config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['S3_BUCKET_NAME'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,14 +73,5 @@ Staff::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV['AWS_BUCKET'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
-
   config.eager_load = true
 end
