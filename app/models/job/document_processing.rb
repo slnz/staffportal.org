8f1d1f1deb2db78bbde2cae53b86802ec596e77c
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-02-05 12:38:39 +1300

    DRY and SRP document processing

--- app/models/job/document_processing.rb
+++ app/models/job/document_processing.rb
@@ -4,34 +4,52 @@ class Job
     @queue = :statused
 
     def perform
-      self.class.transfer_and_cleanup(options['document_id'])
+      load_s3_bucket
+      load_document
+      load_url_data
+      transfer_document
+      cleanup_s3
+      cleanup_document
     end
 
-    def post_process_required?(document)
-      /^(image|(x-)?application)\/(bm``p|gif|jpeg|jpg|pjpeg|png|x-png)$/
-        .match(document.upload_content_type).present?
+    private
+
+    def load_document
+      @document ||= User::Document.find(options['document_id'])
     end
 
-    def self.transfer_and_cleanup(id)
-      document = User::Document.find(id)
-      return if document.processed?
-      direct_upload_url_data = document.url_data
-      s3 = AWS::S3.new
+    def load_s3_bucket
+      @s3_bucket ||= AWS::S3.new.buckets[Rails.configuration.aws[:bucket]]
+    end
 
-      if document.post_process_required?(document)
-        document.upload = URI.parse(URI.escape(document.direct_upload_url))
-      else
-        document.paperclip_file_path =
-          "documents/uploads/#{id}/original/#{direct_upload_url_data[:filename]}"
-        s3.buckets[Rails.configuration.aws[:bucket]]
-          .objects[document.paperclip_file_path].copy_from(direct_upload_url_data[:path])
+    def load_url_data
+      @url_data ||= @document.url_data
+    end
+
+    def transfer_document
+      return if @document.processed?
+      if post_process_required?
+        return @document.upload =
+          URI.parse(URI.escape(document.direct_upload_url))
       end
+      @document.paperclip_file_path =
+        "documents/uploads/#{id}/original/#{@url_data[:filename]}"
+    end
 
-      document.processed = true
-      document.save
+    def cleanup_s3
+      @s3_bucket.objects[@document.paperclip_file_path]
+        .copy_from(@url_data[:path])
+      @s3_bucket.objects[@url_data[:path]].delete
+    end
 
-      s3.buckets[Rails.configuration.aws[:bucket]]
-        .objects[direct_upload_url_data[:path]].delete
+    def cleanup_document
+      @document.processed = true
+      @document.save
+    end
+
+    def post_process_required?
+      /^(image|(x-)?application)\/(bm``p|gif|jpeg|jpg|pjpeg|png|x-png)$/
+        .match(@document.upload_content_type).present?
     end
   end
 end
