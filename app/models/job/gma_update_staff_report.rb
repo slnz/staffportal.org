6805dee246cc4bef3e80f4fe025ce6d4801ca3c9
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-02-17 09:23:18 +1300

    gma publish fixes

--- app/models/job/gma_update_staff_report.rb
+++ app/models/job/gma_update_staff_report.rb
@@ -1,6 +1,8 @@
 class Job
   class GmaUpdateStaffReport < Job
     include Resque::Plugins::Status
+    include Job::Concerns::GmaApi
+
     def perform
       publish(Gma::StaffReport.find(options['gma_staff_report_id']))
     end
