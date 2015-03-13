4e542ca7ebc9701e46e0e183d33a320a284139af
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-03-13 13:49:41 +1300

    organization class fix

--- app/models/gma/organization.rb
+++ app/models/gma/organization.rb
@@ -1,8 +1,12 @@
 class Gma
   class Organization < ActiveRecord::Base
     self.table_name = 'gma_organizations'
-    has_many :gma_staff_reports, dependent: :destroy
-    has_many :gma_memberships, dependent: :destroy, class_name: 'Gma::Membership'
+    has_many :gma_staff_reports,
+             dependent: :destroy,
+             class_name: 'Gma::StaffReport'
+    has_many :gma_memberships,
+             dependent: :destroy,
+             class_name: 'Gma::Membership'
     has_many :users, through: :gma_memberships
   end
 end
