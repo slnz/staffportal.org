100a17e5d74b217ccf87ed26269b9e49c8e331b6
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-11-20 13:02:40 +1300

    Revert "remove require from initialiser"
    This reverts commit bd681e2e99d43da058a87ff44fd4285e75ae7a98.

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,3 +1,4 @@
+require 'aws-sdk'
 # Rails.configuration.aws is used by AWS, Paperclip, and S3DirectUpload
 Rails.configuration.aws = YAML.load(ERB.new(File.read("#{Rails.root}/config/aws.yml")).result)[Rails.env].symbolize_keys!
 AWS.config(logger: Rails.logger)
