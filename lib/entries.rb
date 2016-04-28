27d1be731a931a3cf2d9b91cc2f20e5c21e0ae72
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-04-28 12:18:45 +1200

    update entries script

--- lib/entries.rb
+++ lib/entries.rb
@@ -1,4 +1,4 @@
 Account::Record.includes(:category, :account).all.map { |r|
   r.amount = 0 - r.amount if r.category_id == 161
-  puts "#{r.date.strftime('%m/%d/%Y')},#{r.category.try(:code)},#{r.account.try(:code)},#{r.amount},#{r.id},\"#{r.reason}\""
+  puts "#{r.date.strftime('%m/%d/%Y')},#{r.category.try(:code)},#{r.account.try(:code)},#{r.amount},#{r.id},\"#{r.reason.gsub('"', '\"')}\""
 }
