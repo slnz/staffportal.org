d1913cb73211913fbbcc0e34b5647acddc29d841
Tataihono Nikora <tataihono.nikora@gmail.com>
2016-04-28 14:17:35 +1200

    fix nil case for sub

--- lib/entries.rb
+++ lib/entries.rb
@@ -1,5 +1,5 @@
 Account::Record.includes(:category, :account).all.map { |r|
   r.amount = 0 - r.amount if r.category_id == 161
-  puts "#{r.date.strftime('%m/%d/%Y')},#{r.category.try(:code)},#{r.account.try(:code)},#{r.amount},#{r.id},\"#{r.reason.gsub('"', '\'')}\"" unless r.category && r.account
+  puts "#{r.date.strftime('%m/%d/%Y')},#{r.category.try(:code)},#{r.account.try(:code)},#{r.amount},#{r.id},\"#{r.reason.try(:gsub, '"', '\'')}\"" if r.category && r.account
   nil
 }
